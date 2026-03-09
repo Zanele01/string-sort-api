fa77a7eb8e48e1e5dd293438b8586a084f6d5b6e
Zanele01 <zanele.malinga2@gmail.com>
2026-03-09 11:29:16 +0200

    Fixed validator GET endpount

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class InputData(BaseModel):
-    data: str
-
-@app.post("/")
-def sort_string(input: InputData):
-    sorted_chars = sorted(input.data)
+@app.get("/")
+def sort_string(data: str):
+    sorted_chars = sorted(data)
     return {"word": sorted_chars}
